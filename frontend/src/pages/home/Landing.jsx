23e99fdfd65fa409bd24dcbcf9308b73bcb2421d
Plasmarim <Thanapat_petch@hotmail.com>
2025-10-25 16:06:37 +0700

    remove . from text

--- frontend/src/pages/home/Landing.jsx
+++ frontend/src/pages/home/Landing.jsx
@@ -8,7 +8,7 @@ function Landing() {
       <NavigationBar />
 
       <div className={`flex mt-[60px] ml-[60px] text-[52px] text-black font-[700]`}>Co-working Space Reservation</div>
-      <div className={`flex mt-[60px] ml-[60px] text-[24px] text-gray-500`}>Please choose from option below.</div>
+      <div className={`flex mt-[60px] ml-[60px] text-[24px] text-gray-500`}>Please choose from option below</div>
       <div className={`flex flex-col mt-[20px] ml-[60px] gap-6`}>
         <KeyboardButton width={"380px"} height={"90px"} label={"Create new reservation"} fontSize={"22px"}/>
         <KeyboardButton width={"380px"} height={"90px"} label={"View my reservation"} fontSize={"22px"}/>
