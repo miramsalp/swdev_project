4f9651bc22226c4f5e5c23d8b3c291e26dee3bf0
Kritsada Chan-inn <bankth99@gmail.com>
2025-11-19 19:39:45 +0700

    add favico and register handle

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -18,14 +18,20 @@ exports.register = async (req, res, next) => {
     try {
         const { name, email, password, phone } = req.body
 
+        const user = await User.findOne({email: email});
+        if(user) {
+            res.status(400).json({ success: false, msg: "This email has already been registered"});
+            return;
+        }
         // Create user
-        const user = await User.create({
+        const newUser = await User.create({
             name,
             email,
             password,
             phone,
             role: 'user'
         })
+
 
         sendTokenResponse(user, 200, res);
     } catch (error) {
